ea2d2c3818bd162a5fc8c68dc6e0323385fdcc9c
Aaron Adrid <119366745+aaronadrid-isv@users.noreply.github.com>
2023-04-05 13:31:26 -0700

    relocate and isolate s3-deploy utility

--- utils/s3-deploy/s3-deploy.mjs
+++ utils/s3-deploy/s3-deploy.mjs
@@ -11,16 +11,46 @@ import decompress from 'decompress';
 import path from 'path';
 import * as fsPromise from 'fs/promises';
 import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
 
 const COMMAND_LINE_ARG_OPTIONS = [
-  { name: 'region', alias: 'r', type: String },
-  { name: 'bucket', alias: 'b', type: String }
-  { name: 'paths', type: String, multiple: true, defaultOption: true },
-  { name: 'delete', type: String, alias: 'd', defaultValue: 'true' },
-  { name: 'verbose', type: Boolean, alias: 'v'},
-  { name: 'quiet', type: Boolean, alias: 'q'}
+  { name: 'help', type: Boolean, alias: 'h', description: 'Display this usage guide'},
+  { name: 'region', alias: 'r', type: String, description: '{bold {italic REQUIRED:}} AWS region profile to use defined in ini credentials' },
+  { name: 'bucket', alias: 'b', type: String, description: '{bold {italic REQUIRED:}} S3 bucket name' },
+  {
+    name: 'paths',
+    type: String,
+    multiple: true,
+    defaultOption: true,
+    description: '{bold {italic REQUIRED}} Path to zip file and an {italic OPTIONAL} extract to path',
+    typeLabel: '<zip_file_path> [extract_to_path]'
+  },
+  { name: 'subfolder', type: String, alias: 's', defaultValue: 'rapid-dicom-viewer', description: 'S3 bucket subfolder.\n{italic Default: rapid-dicom-viewer}'},
+  { name: 'nodelete', type: Boolean, alias: 'n', description: 'Do not delete existing files in the S3 subfolder' },
+  { name: 'verbose', type: Boolean, alias: 'v', description: 'Verbose console logging'},
+  { name: 'quiet', type: Boolean, alias: 'q', description: 'Minimal console logging'},
+  { name: 'keep', type: Boolean, alias: 'k', description: 'Keep extracted files instead of automatically deleting'}
 ];
 
+const USAGE_CONFIG = [
+  {
+    header: '{underline rapid-dicom-viewer S3 Deploy Script}',
+    content: 'A nodejs utility script to streamline cloud hosted rapid-dicom-viewer deployments to an S3 environment'
+  },
+  {
+    header: '{underline Usage}',
+    content: `  node s3-deploy.mjs <zip_file_path> [extract_to_path] \n    --region <aws_region_profile> --bucket <bucket_name> [OPTIONS]`,
+    raw: true
+  },
+  {
+    header: '{underline Options}',
+    optionList: COMMAND_LINE_ARG_OPTIONS
+  }
+];
+
+const USAGE = commandLineUsage(USAGE_CONFIG);
+
+// Bit flags for log level processing
 const LogLevel = {
   None: 0x0,
   QuietBypass: 0x1,
@@ -31,20 +61,6 @@ const LogLevel = {
 
 const OPTIONS = commandLineArgs(COMMAND_LINE_ARG_OPTIONS);
 
-const REGION_PROFILE = OPTIONS.region ?? 'isv-sandpit3';
-
-const client = new S3Client({
-  credentials: fromIni({ profile: REGION_PROFILE })
-});
-
-async function unzipFile(sourcePath, destinationPath) {
-  return await decompress(sourcePath, destinationPath);
-}
-
-const BUCKET_NAME = OPTIONS.bucket ?? 'rapid-webapp-sandpit3';
-const S3_DIRECTORY = 'rapid-dicom-viewer';
-const ZIP_FILE_PATH = OPTIONS.paths[0];
-const DELETE_PREVIOUS = OPTIONS.delete === 'true';
 const VERBOSE = OPTIONS.verbose;
 const QUIET = OPTIONS.quiet;
 
@@ -52,7 +68,21 @@ const LOG_LEVEL = VERBOSE ?
   LogLevel.QuietBypass | LogLevel.Normal | LogLevel.Verbose | LogLevel.Error :
   QUIET ?
     LogLevel.QuietBypass | LogLevel.Error :
-    LogLevel.Normal | LogLevel.Error;
+    LogLevel.QuietBypass | LogLevel.Normal | LogLevel.Error;
+
+const REGION_PROFILE = OPTIONS.region ?? 'isv-sandpit3';
+const BUCKET_NAME = OPTIONS.bucket;
+const S3_DIRECTORY = OPTIONS.subfolder;
+const ZIP_FILE_PATH = OPTIONS.paths?.[0];
+const EXTRACT_DIRECTORY = OPTIONS.paths?.[1] ?? path.dirname(ZIP_FILE_PATH ?? '.');
+const DELETE_PREVIOUS = !OPTIONS.nodelete;
+const KEEP_EXTRACTED = OPTIONS.keep;
+
+
+if (OPTIONS.help || !REGION_PROFILE || !BUCKET_NAME || !ZIP_FILE_PATH) {
+  log(USAGE, LogLevel.QuietBypass);
+  process.exit(0);
+}
 
 const EXCLUDE_PATH_FLAGS = {
   '__MACOSX': true,
@@ -72,6 +102,14 @@ const CONTENT_TYPE_BY_EXT = {
   'wasm': 'application/wasm',
   'xml': 'application/xml'
 };
+
+const client = new S3Client({
+  credentials: fromIni({ profile: REGION_PROFILE })
+});
+
+async function unzipFile(sourcePath, destinationPath) {
+  return await decompress(sourcePath, destinationPath);
+}
 
 function getContentType(filePath) {
   const extension = path.extname(filePath).replace('.', '').toLowerCase();
@@ -133,35 +171,42 @@ async function deletePrevious() {
 }
 
 async function deployZip() {
-  const extractDirectory = path.dirname(ZIP_FILE_PATH);
-  const files = await unzipFile(ZIP_FILE_PATH, extractDirectory);
-
+  const files = await unzipFile(ZIP_FILE_PATH, EXTRACT_DIRECTORY);
+  const {sep} = path;
+  const pathPrefixRegex = new RegExp(`^(package${sep})?dist${sep}`);
   for (let file of files) {
     // console.log(`${file.type}: ${file.path}`)
 
     if (file.type === 'directory' ||
         Object.keys(EXCLUDE_PATH_FLAGS).some(path => file.path.match(path))) {
-      log(`***Excluding file: ${file.path}`, LogLevel.Verbose);
+      log(`***Excluding file/dir entry: ${file.path}`, LogLevel.Verbose);
       continue;
     }
+
+    const extractedFilePath = path.join(EXTRACT_DIRECTORY, file.path);
+
     const command = new PutObjectCommand({
       Bucket: BUCKET_NAME,
-      Key: path.join(S3_DIRECTORY, file.path.replace('dist/', '')),
+      Key: path.join(S3_DIRECTORY, file.path.replace(pathPrefixRegex, '')),
       ContentType: getContentType(file.path),
-      Body: await fsPromise.readFile(path.join(extractDirectory, file.path)),
+      Body: await fsPromise.readFile(extractedFilePath),
       CacheControl: file.path.endsWith('index.html') ? 'no-cache' : undefined
       //Body: path.join(extractDirectory, file.path)
     });
 
     try {
       log(`Uploading file: ${command.input.Key} - start`, LogLevel.Verbose);
-      const result = await client.send(command);
+      await client.send(command);
       log(`Uploading file: ${command.input.Key} - complete`, LogLevel.Verbose);
     } catch (e){
       log(`Error encoutered attempting to upload: ${command.input.Key}`, LogLevel.Error, e);
     }
-
   }
+
+  if (!KEEP_EXTRACTED) {
+    await fsPromise.rm(path.join(EXTRACT_DIRECTORY, files[0].path), { recursive: true });
+  }
+
 }
 
 function log(message, level, data) {
