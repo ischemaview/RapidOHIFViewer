8b80f960eb055e3d6162d116fb450126566c2a81
Hemant Patel <115255387+HemantPatel30@users.noreply.github.com>
2023-06-27 10:12:33 -0500

    Enhance CT - Fix Image Rendering issue (#46)

--- platform/core/src/classes/ImageSet.ts
+++ platform/core/src/classes/ImageSet.ts
@@ -89,47 +89,48 @@ class ImageSet {
   sortByImagePositionPatient() {
     const images = this.rawImages;
     const referenceImagePositionPatient = _getImagePositionPatient(images[0]);
-
-    const refIppVec = new Vector3(
-      referenceImagePositionPatient[0],
-      referenceImagePositionPatient[1],
-      referenceImagePositionPatient[2]
-    );
-
     const ImageOrientationPatient = _getImageOrientationPatient(images[0]);
 
-    const scanAxisNormal = new Vector3(
-      ImageOrientationPatient[0],
-      ImageOrientationPatient[1],
-      ImageOrientationPatient[2]
-    ).cross(
-      new Vector3(
-        ImageOrientationPatient[3],
-        ImageOrientationPatient[4],
-        ImageOrientationPatient[5]
-      )
-    );
+    if (referenceImagePositionPatient && ImageOrientationPatient) {
+      const refIppVec = new Vector3(
+        referenceImagePositionPatient[0],
+        referenceImagePositionPatient[1],
+        referenceImagePositionPatient[2]
+      );
 
-    const distanceImagePairs = images.map(function(image) {
-      const ippVec = new Vector3(..._getImagePositionPatient(image));
-      const positionVector = refIppVec.clone().sub(ippVec);
-      const distance = positionVector.dot(scanAxisNormal);
+      const scanAxisNormal = new Vector3(
+        ImageOrientationPatient[0],
+        ImageOrientationPatient[1],
+        ImageOrientationPatient[2]
+      ).cross(
+        new Vector3(
+          ImageOrientationPatient[3],
+          ImageOrientationPatient[4],
+          ImageOrientationPatient[5]
+        )
+      );
 
-      return {
-        distance,
-        image,
-      };
-    });
+      const distanceImagePairs = images.map(function(image) {
+        const ippVec = new Vector3(..._getImagePositionPatient(image));
+        const positionVector = refIppVec.clone().sub(ippVec);
+        const distance = positionVector.dot(scanAxisNormal);
 
-    distanceImagePairs.sort(function(a, b) {
-      return b.distance - a.distance;
-    });
+        return {
+          distance,
+          image,
+        };
+      });
 
-    const sortedImages = distanceImagePairs.map(a => a.image);
+      distanceImagePairs.sort(function(a, b) {
+        return b.distance - a.distance;
+      });
 
-    images.sort(function(a, b) {
-      return sortedImages.indexOf(a) - sortedImages.indexOf(b);
-    });
+      const sortedImages = distanceImagePairs.map(a => a.image);
+
+      images.sort(function(a, b) {
+        return sortedImages.indexOf(a) - sortedImages.indexOf(b);
+      });
+    }
   }
 }
 
