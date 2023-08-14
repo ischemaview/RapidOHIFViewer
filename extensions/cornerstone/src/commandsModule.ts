645b63ee2f8546c68a5fecd71a28cecbab7ddf7b
Hemant Patel <hemant.patel@citiustech.com>
2023-08-14 14:13:54 -0500

    RDV-524 missing code

--- extensions/cornerstone/src/commandsModule.ts
+++ extensions/cornerstone/src/commandsModule.ts
@@ -550,6 +550,9 @@ function commandsModule({ servicesManager, commandsManager }) {
       if (crosshairsToolInstance) {
         crosshairsToolInstance.resetAnnotations();
       }
+      const rapidIconState = stateSyncService.getState().rapidIconState;
+      rapidIconState.zoomValueChange = {};
+      stateSyncService.store({ rapidIconState });
     },
     scaleViewport: ({ direction }) => {
       const enabledElement = _getActiveViewportEnabledElement();
